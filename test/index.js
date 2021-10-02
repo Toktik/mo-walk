35cc14c3c6ac931a37c42de7dac809097d436f3c
Devin Ivy <devin@bigroomstudios.com>
2021-10-02 15:13:31 -0400

    Initial commit 🍱

---
+++ test/index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// Load modules
+
+const Lab = require('@hapi/lab');
+const Code = require('@hapi/code');
+const MoWalk = require('..');
+
+// Test shortcuts
+
+const { describe, it } = exports.lab = Lab.script();
+const { expect } = Code;
+
+const internals = {};
+
+describe('MoWalk', () => {
+
+});
